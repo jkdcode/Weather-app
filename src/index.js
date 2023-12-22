db82fc3a3249bc587329b8a22a640c26a6d1d403
jkdcode <jk.didriksen@gmail.com>
2023-12-22 16:45:27 +0100

    changes to search form to avoid button overlap on small screens

--- src/index.js
+++ src/index.js
@@ -7,22 +7,17 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-
   let iconElement = document.querySelector("#icon");
-
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
-
-  cityElement.innerHTML = response.data.city;
-
   let weatherDescription = response.data.condition.description;
   let capitalizedDescription =
     weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
-
+  cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = capitalizedDescription;
   timeElement.innerHTML = formatDate(date);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 }
 
 function formatDate(date) {
