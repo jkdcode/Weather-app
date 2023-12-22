893e4bf4aafb2ecf5f04de9b78c31ebffa653211
jkdcode <jk.didriksen@gmail.com>
2023-12-22 12:59:02 +0100

    Added API functionality for wind speed and humidity

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
