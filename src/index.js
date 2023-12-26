194f18fccb26babfc504bf3943ecbe2a69d65875
jkdcode <jk.didriksen@gmail.com>
2023-12-26 16:45:22 +0100

    Small design adjustment

--- src/index.js
+++ src/index.js
@@ -69,6 +69,7 @@ function displayForecast() {
         <div class="weather-forecast-icon">⛅</div>
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max">18°</span>
+          <span class="weather-forecast-temperature-spacer"> / </span>
           <span class="weather-forecast-temperature-min">12°</span>
         </div>
       </div>
